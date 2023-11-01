5245e1c8afa86f3db020cc8112c2139db25a17cc
Nicolas Spijkerman <nicolas.spijkerman@epitech.eu>
2023-11-01 21:02:42 +0100

    add function

--- src/strain/ParserAST.hs
+++ src/strain/ParserAST.hs
@@ -20,10 +20,11 @@ data AstType
 data AstBinding
   = Bound    String  AstType Ast
   | ReBound  String  Ast
+  | Arg      String  AstType
   | BConst   String  AstType Ast
   | Enum     String [AstBinding]
   | Struct   String [AstBinding]
-  | Function String [AstBinding] AstType Ast
+  | Function String [AstBinding] AstType [Ast]
   deriving (Show, Eq)
 
 data Operator = Add | Sub | Mul | Div | Mod | Not | And | Or | Gt | Lt | Eq deriving (Show, Eq)
@@ -195,6 +196,8 @@ parseBound acc (BSection maybeBinding : next) =
             TokTyFloat  -> Just $ Binding $ Bound name TyFloat  vv
             TokTyChar   -> Just $ Binding $ Bound name TyChar   vv
             TokTyInt    -> Just $ Binding $ Bound name TyInt    vv
+            TokTyBool   -> Just $ Binding $ Bound name TyBool   vv
+            TokTyVoid   -> Just $ Binding $ Bound name TyVoid   vv
             _           -> Nothing
         _ -> Nothing
     parseBound' (T TokConst : T (TokIde name) : T TokCol : T mtype : T TokAsgn : value) =
@@ -205,6 +208,8 @@ parseBound acc (BSection maybeBinding : next) =
             TokTyFloat  -> Just $ Binding $ BConst name TyFloat  vv
             TokTyChar   -> Just $ Binding $ BConst name TyChar   vv
             TokTyInt    -> Just $ Binding $ BConst name TyInt    vv
+            TokTyBool   -> Just $ Binding $ Bound name TyBool   vv
+            TokTyVoid   -> Just $ Binding $ Bound name TyVoid   vv
             _           -> Nothing
         _ -> Nothing
     parseBound' _ = Nothing
@@ -233,4 +238,41 @@ parseIncrAndBound _ _ = Nothing
 
 
 parseFunction :: AstParser
+parseFunction acc (T TokFun : T (TokIde funName) : Prths args : T TokRetTy : T tokType : Braces body : next) =
+  case parseArgs [] args of
+    Nothing -> Nothing
+    Just va ->
+      case getAst body of
+        Left  _ -> Nothing
+        Right b ->
+          case tokType of
+            TokTyString -> Just (acc ++ [Binding $ Function funName va TyString b], next)
+            TokTyFloat  -> Just (acc ++ [Binding $ Function funName va TyFloat  b], next)
+            TokTyChar   -> Just (acc ++ [Binding $ Function funName va TyChar   b], next)
+            TokTyInt    -> Just (acc ++ [Binding $ Function funName va TyInt    b], next)
+            TokTyBool   -> Just (acc ++ [Binding $ Function funName va TyBool   b], next)
+            TokTyVoid   -> Just (acc ++ [Binding $ Function funName va TyVoid   b], next)
+            _           -> Nothing
+  where
+    parseArgs :: [AstBinding] -> [BExpr] -> Maybe [AstBinding]
+    parseArgs [] [] = Just []
+    parseArgs a [T (TokIde argName), T TokCol, T tokT] =
+      case tokT of
+        TokTyString -> Just (a ++ [Arg argName TyString])
+        TokTyFloat  -> Just (a ++ [Arg argName TyString])
+        TokTyChar   -> Just (a ++ [Arg argName TyString])
+        TokTyInt    -> Just (a ++ [Arg argName TyString])
+        TokTyBool   -> Just (a ++ [Arg argName TyString])
+        TokTyVoid   -> Just (a ++ [Arg argName TyString])
+        _           -> Nothing
+    parseArgs a (T (TokIde argName) : T TokCol : T tokT : T TokCom : n) =
+      case tokT of
+        TokTyString -> parseArgs (a ++ [Arg argName TyString]) n
+        TokTyFloat  -> parseArgs (a ++ [Arg argName TyString]) n
+        TokTyChar   -> parseArgs (a ++ [Arg argName TyString]) n
+        TokTyInt    -> parseArgs (a ++ [Arg argName TyString]) n
+        TokTyBool   -> parseArgs (a ++ [Arg argName TyString]) n
+        TokTyVoid   -> parseArgs (a ++ [Arg argName TyString]) n
+        _           -> Nothing
+    parseArgs _ _ = Nothing
 parseFunction _ _ = Nothing
